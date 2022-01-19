ebe84e7caab7b5d4061c120e02156e51da76a7f1
German Ruiz <germanrdarco@gmail.com>
2022-01-19 21:51:16 +0100

    19/01/2022

--- Segundo_trimestre/Proyecto_VUE/index.js
+++ Segundo_trimestre/Proyecto_VUE/index.js
@@ -7,18 +7,55 @@ const tareas = {
         return{
             listaTareas: [],
             tarea: '',
-            prioridad: ''
+            prioridad:'',
+            clase_true: 'completado',
+            clase_false: 'incompleto'
         }
     },
     methods:{
         agregar(){
-            this.listaTareas.push({nombre:this.tarea, prioridad:this.prioridad});
+            this.listaTareas.push(
+                {
+                    nombre: this.tarea,
+                    fecha: new Date(),
+                    prioridad: this.prioridad,
+                    estado:false
+                }
+            );
+            this.tarea = '';
+
         },
+        completar(index){
+            this.listaTareas[index].estado = !this.listaTareas[index].estado;
+            
+            this.updateLocalStorage();
+        },
+        borrar(index){
+            this.listaTareas.splice(index, 1);
+            this.updateLocalStorage();
+        },
+        updateLocalStorage(){
+            localStorage.all = JSON.stringify(this.listaTareas);
+        },
+        borrarCompletadas(){
+            arr =[];
+            this.listaTareas.forEach(element => {
+                if(element.estado == false){
+                    arr.push(element);
+                }
+            });
+            this.listaTareas = arr;
+            this.updateLocalStorage();
+        }
     },
     computed:{
-        
+        incompletos(){
+            let num_incompletos = this.listaTareas.filter(tarea => !tarea.estado).length;
+            return num_incompletos;
+        }
     },
     mounted(){
-
+        if(localStorage.all)
+            this.listaTareas = JSON.parse(localStorage.all);
     }
 }
